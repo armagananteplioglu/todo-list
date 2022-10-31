d97c8cfafc8549318d1e289863989c86672dfd9b
Armagan Anteplioglu <armagananteplioglu@gmail.com>
2022-10-31 05:53:02 +0200

    quick save

--- src/main-page.js
+++ src/main-page.js
@@ -13,3 +13,88 @@ const newProjectButton = document.createElement("div")
 newProjectButton.setAttribute("id", "projectButton")
 newProjectButton.textContent = "+ New Project"
 
+let projectsArray = [];
+
+function Projects(name) {
+  this.name = name;
+}
+/*
+const createProject = () => {
+    let projectName = prompt("Set new project name:");
+    const project = new Projects(projectName);
+    if (projectName === null) {
+      return;
+    } else if (projectName === "") {
+      alert("The project name can not be empty.");
+      return;
+    } else {
+      projectsArray.push(project);
+      console.log(projectsArray);
+      displayProjects();
+      addProjectFunctionality();
+    }
+};
+
+const displayProjects = () => {
+    projectPane.innerHTML = "";
+    projectsArray.forEach((element) => {
+      const projectContainer = document.createElement("div");
+      projectContainer.setAttribute("class", "projects");
+      projectContainer.append(element.name);
+      projectPane.append(projectContainer);
+    });
+};
+
+const addProjectFunctionality = () => {
+    const deleteButton = document.createElement("img");
+    deleteButton.setAttribute("id", "delete");
+    deleteButton.src = "../components/multiplication.svg"
+    projectContainer.append(deleteButton)
+};
+*/
+
+const createProject = {
+    exec: () => {
+        createProject.makeProjectName()
+        createProject.createContainer(createProject.projectDeleteButton())
+        console.log(createProject.projectDeleteButton())
+    },
+    makeProjectName: () => {
+        let projectName = prompt("Set new project name:");
+        const project = new Projects(projectName);
+        if (projectName === null) {
+          return;
+        } else if (projectName === "") {
+          alert("The project name can not be empty.");
+          return;
+        } else {
+          projectsArray.push(project);
+        }
+    },
+    createContainer: (feature1) => {
+        projectPane.innerHTML = "";
+        projectsArray.forEach((element) => {
+            const projectContainer = document.createElement("div");
+            projectContainer.setAttribute("class", "projects");
+            projectContainer.append(element.name, feature1);
+            projectPane.append(projectContainer);
+        });
+    },
+    projectDeleteButton: () => {
+        const deleteButton = document.createElement("img");
+        deleteButton.setAttribute("class", "delete");
+        deleteButton.src = "../components/multiplication.svg"
+        return deleteButton
+    },
+    something: () => {
+        console.log("lol")
+    }
+}
+
+newProjectButton.addEventListener("click", createProject.exec);
+
+container.addEventListener("click", function(e) {
+  if (e.target.className == "delete") {
+    createProject.something()
+  }
+})
